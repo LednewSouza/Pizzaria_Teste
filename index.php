58f495b4319503bb48f523e564a576f047d71e2a
LednewSouza <157215383+LednewSouza@users.noreply.github.com>
2024-01-27 21:58:23 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+include 'header.php'; 
+
+?>
+
+<body>
+<table>
+    <tr>
+        <td>
+            <img scr=''>
+            <h1></h1>
+            <p></p>
+
+            <div>
+                <label for="G">Grande</label>
+                <input type="radio" id="G" name="tamanho" value="">
+            </div>
+        </td>
+    </tr>
+</table>
+<script>
+    console.log('pio')
+</script>
+</body>
